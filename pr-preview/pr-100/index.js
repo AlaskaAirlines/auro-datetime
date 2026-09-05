7c28bf3ebd1213f0aed931c89de4b4a5a98db556
jason-capsule42 <jason-capsule42@users.noreply.github.com>
2026-09-05 01:19:20 +0000

    Remove preview for PR 100 🛬

--- pr-preview/pr-100/index.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable jsdoc/require-jsdoc, no-magic-numbers, no-param-reassign */
-import { AuroDatetime } from "../src/index";
-
-AuroDatetime.register();
-AuroDatetime.register("custom-datetime");
