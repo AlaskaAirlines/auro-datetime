eab8a83182ead4b38f67f9af8a4db7586b92733b
jason-capsule42 <jason-capsule42@users.noreply.github.com>
2026-09-23 19:53:28 +0000

    Deploy preview for PR 106 🛫

---
+++ pr-preview/pr-106/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable jsdoc/require-jsdoc, no-magic-numbers, no-param-reassign */
+import { AuroDatetime } from "../src/index";
+
+AuroDatetime.register();
+AuroDatetime.register("custom-datetime");
